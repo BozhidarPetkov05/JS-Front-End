808c9aadb3d234daf5ae230bac435564665ddbd7
Bozhidar Petkov <xidz01bog@gmail.com>
2024-04-05 20:43:58 +0300

    All done

--- DOM Manipulation and Events/P03. Add Delete/app.js
+++ DOM Manipulation and Events/P03. Add Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    const textElement = document.getElementById('newItemText');
+    const tableElement = document.getElementById('items');
+    
+    const newElement = document.createElement('li');
+    newElement.textContent = textElement.value; 
+    
+    const deleteLinkElement = document.createElement('a');
+    deleteLinkElement.textContent = '[Delete]';
+    deleteLinkElement.href = '#';
+
+    deleteLinkElement.addEventListener('click', () => {
+        newElement.remove();
+    });
+
+    
+    newElement.appendChild(deleteLinkElement);
+    tableElement.appendChild(newElement);
+    textElement.value = '';
 }
+
--- DOM Manipulation and Events/P07. Shopping Cart/solution.js
+++ DOM Manipulation and Events/P07. Shopping Cart/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-   //TODO...
+    const addProductButtonElements = document.querySelectorAll('button.add-product');
+    const textAreaElement = document.querySelector('textarea');
+    const checkoutElement = document.querySelector('button.checkout');
+    
+    let products = {};
+    let total = 0;
+    
+    for (const buttonElement of addProductButtonElements) {
+        const productElement = buttonElement.parentElement.parentElement;
+        buttonElement.addEventListener('click', (event => {
+            const productName = productElement.querySelector('.product-title').textContent;
+            const productPrice = Number(productElement.querySelector('.product-line-price').textContent);
+            textAreaElement.textContent += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`;
+            total += productPrice;
+            products[productName] = true;
+        }))
+    }
+    checkoutElement.addEventListener('click', (event) => {
+        const list = Object.keys(products).join(', ');
+        textAreaElement.textContent += `You bought ${list} for ${total.toFixed(2)}.`
+        checkoutElement.setAttribute('disabled', 'disabled');
+        for (const button of addProductButtonElements) {
+            button.setAttribute('disabled', 'disabled');
+        }
+    });
 }
