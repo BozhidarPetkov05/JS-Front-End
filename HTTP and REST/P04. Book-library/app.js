7d955c1f9054cdd6aaa086402a5e1ce72f06cd23
Bozhidar Petkov <xidz01bog@gmail.com>
2024-04-07 20:26:36 +0300

    P04 Almost done

--- HTTP and REST/P04. Book-library/app.js
+++ HTTP and REST/P04. Book-library/app.js
@@ -1,5 +1,41 @@
 function attachEvents() {
-  // TODO:
+	const titleElement = document.querySelector('input[type=text][name=title]');
+	const authorElement = document.querySelector('input[type=text][name=author]');
+	const submitButtonElement = document.querySelector('#form button');
+	const bookTableBodyElement = document.querySelector('#books tbody')
+	submitButtonElement.addEventListener('click', (e) => {
+		const bookTrElement = createBookElement(titleElement.value, authorElement.value);
+		
+		bookTableBodyElement.appendChild(bookTrElement);
+		
+		titleElement.value = '';
+		authorElement.value = '';
+	})
+}
+
+function createBookElement(title, author) {
+	const editButton = document.createElement('button');
+	const deleteButton = document.createElement('button');
+
+	editButton.textContent = 'Edit';
+	deleteButton.textContent = 'Delete';
+
+	const buttonsTdElement = document.createElement('td');
+	buttonsTdElement.appendChild(editButton);
+	buttonsTdElement.appendChild(deleteButton);
+
+	const titleTdElement = document.createElement('td');
+	titleTdElement.textContent = title;
+
+	const authorTdElement = document.createElement('td');
+	authorTdElement.textContent = author;
+
+	const bookTrElement = document.createElement('tr');
+	bookTrElement.appendChild(titleTdElement);
+	bookTrElement.appendChild(authorTdElement);
+	bookTrElement.appendChild(buttonsTdElement);
+
+	return bookTrElement;
 }
 
 attachEvents();
