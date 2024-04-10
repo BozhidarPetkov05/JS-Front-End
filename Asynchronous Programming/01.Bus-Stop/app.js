73e9e4380df235917a01d06924a62b2ff5dbbbe1
Bozhidar Petkov <xidz01bog@gmail.com>
2024-04-10 13:36:44 +0300

    2/3 Done

--- Asynchronous Programming/01.Bus-Stop/app.js
+++ Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,25 @@
 function getInfo() {
-    // TODO:
+    const baseURL = 'http://localhost:3030/jsonstore/bus/businfo';
+    
+    const stopIdElement = document.getElementById('stopId');
+    const stopNameElement = document.getElementById('stopName');
+    const busesElement = document.getElementById('buses');
+
+    const stopId = stopIdElement.value;
+    fetch(`${baseURL}/${stopId}`)
+        .then(res => res.json())
+        .then(data => {
+            stopNameElement.textContent = data.name;
+            busesElement.textContent = '';
+            for (const busId in data.buses) {
+                const liElement = document.createElement('li');
+                liElement.textContent = `Bus ${busId} arrives in ${data.buses[busId]} minutes`;
+                busesElement.appendChild(liElement);
+            }
+            
+        })
+        .catch(() => {
+            stopNameElement.textContent = 'Error';
+            busesElement.textContent = '';
+        });
 }
--- Asynchronous Programming/03.Forecaster/app.js
+++ Asynchronous Programming/03.Forecaster/app.js
@@ -1,5 +1,76 @@
 function attachEvents() {
-    // TODO:
+    const baseURL = 'http://localhost:3030/jsonstore/forecaster';
+    const locationElement = document.getElementById('location');
+    const submitButton = document.getElementById('submit');
+    const forecastElement = document.getElementById('forecast');
+    const currentElement = document.getElementById('current');
+    const upcomingElement = document.getElementById('upcoming');
+
+    const weatherSymbol = {
+        'Sunny': '☀',
+        'Partly sunny': '⛅',
+        'Overcast': '☁',
+        'Rain': '☂',
+        'Degrees': '°'
+    }
+
+    submitButton.addEventListener('click', () => {
+        fetch(`${baseURL}/locations`)
+            .then(res => res.json())
+            .then(locationData => {
+                const { code } = locationData.find(location => location.name === locationElement.value);
+
+                return Promise.all([
+                    fetch(`${baseURL}/today/${code}`),
+                    fetch(`${baseURL}/upcoming/${code}`)
+                ]);
+            })
+            .then(responses => Promise.all(responses.map(res => res.json())))
+            .then(([today, upcoming]) => {
+                forecastElement.style.display = 'block';
+                const symbolSpanElement = document.createElement('span');
+                symbolSpanElement.textContent = weatherSymbol[today.forecast.condition];
+                symbolSpanElement.classList.add('condition');
+                symbolSpanElement.classList.add('symbol');
+
+                const anotherSpan = document.createElement('span');
+                anotherSpan.innerHTML = `
+                    <span class="condition">
+                        <span class="forecast-data">${today.name}</span>
+                        <span class="forecast-data">${today.forecast.low}°/${today.forecast.high}°</span>
+                        <span class="forecast-data">${today.forecast.condition}</span>
+                    </span>
+                `;
+
+                const forecastsElement = document.createElement('div');
+                forecastsElement.classList.add('forecasts');
+                forecastsElement.appendChild(symbolSpanElement);
+                forecastsElement.appendChild(anotherSpan);
+                currentElement.appendChild(forecastsElement);
+
+                /*const upcomingSpan = document.createElement('div');
+                upcomingSpan.innerHTML = `<div class="forecast-info">
+                        <span class="upcoming">
+                            <span class="symbol">${upcoming[0]}</span>
+                            <span class="forecast-data"><${upcoming.low}°/${upcoming.high}°/span>
+                            <span class="forecast-data"></span>
+                        </span>
+                        <span class="upcoming">
+                            <span class="symbol"></span>
+                            <span class="forecast-data"></span>
+                            <span class="forecast-data"></span>
+                        </span>
+                        <span class="upcoming">
+                            <span class="symbol"></span>
+                            <span class="forecast-data"></span>
+                            <span class="forecast-data"></span>
+                        </span>
+                    </div>
+                `;*/
+            })
+            .catch(() => console.log('Error'));
+
+    })
 }
 
 attachEvents();
